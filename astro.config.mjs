a753b1bcb56537aa1378b0e0effc1ce4c6a7c7e5
Bennie Rosas <ben@aliencyb.org>
2026-06-23 23:42:28 -0400

    ci: configure GitHub Pages deployment and workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ballpointpenguin.github.io',
+  base: '/tetes-contre-pieds',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr'],
